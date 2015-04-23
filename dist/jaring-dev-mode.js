af78fc719c6b8318c45f7bb6faf7ca90daeee824
煎饼 <crossyou2009@gmail.com>
2015-04-23 11:53:06 +0800

    my javascript lib
    transfer svn.sobird.me repository to git

---
+++ dist/jaring-dev-mode.js
@@ -0,0 +1,71 @@
+/**
+ * JARING - 4 commemorate
+ *
+ * Copyleft 2011, CROSSYOU.CN
+ * Released under LGPL License.
+ *
+ * License: http://dev.crossyou.cn/license
+ *
+ * This file loads the js files from source instead of a merged copy.
+ * 
+ * Version: $Id: jaring-dev-mode.js 12 2015-03-05 04:06:48Z sobird $
+ */
+
+ (function(){
+ 	var scripts = [];
+
+ 	//---------------------------------------------------------------
+ 	function getPath(){
+ 		var nl = document.getElementsByTagName('script');
+		for (var i = 0; i < nl.length; i++) {
+			var src = nl[i].src;
+			if (src && src.indexOf('jaring-dev-mode.js') != -1) {
+				return dirname(dirname(src));
+			}
+		}
+ 	}
+
+ 	function dirname(dirstr){
+ 		var dir = dirstr.substring(0, dirstr.lastIndexOf('/'));
+ 		if (dir) {
+			return dir ;
+		} else {
+			return dirstr;
+		}
+ 	}
+
+ 	function init(scripts){
+ 		var output = '';
+		for (var i = 0; i < scripts.length; i++){
+			output += '<script type="text/javascript" src="' + scripts[i] + '"></script>\n';
+		}
+		document.write(output);
+ 	}
+
+ 	function require(jsfile){
+ 		var base = getPath();
+		scripts.push(base + '/src/' + jsfile);
+	}
+	//----------------------------------------------------------------
+	
+	require('jaring.js');
+	require('core/constant.js');
+	require('core/util.js');
+	require('core/browser.js');
+	require('core/event.js');
+	require('core/Observable.js');
+	require('core/MVCObject.js');
+
+	require('type/Event.js');
+	require('type/Offset.js');
+	require('type/Point.js');
+
+	require('core/dom.js');
+
+	require('widget/Draggable.js');
+
+	//Core 核心文件
+	
+	//-----------------------------------------------------------------
+	init(scripts);
+ })();
---
+++ src/jaring.js
@@ -0,0 +1,23 @@
+/**
+ * Jaring NameSpace
+ *
+ * @author junlong.yang
+ * @version $Id: jaring.js 12 2015-03-05 04:06:48Z sobird $
+ */
+(function(Jaring, win){
+	win.Jaring = win.Jaring || Jaring;
+})({
+		SVNINFO: "$Id: jaring.js 12 2015-03-05 04:06:48Z sobird $",
+		
+		VERSION: "$version$",
+
+		RELEASE: "$release$",
+
+		noConflict: function(){},
+
+		emptyFn: function(){},
+
+		falseFn: function(){
+			return false;
+		}
+	},window);
